f93c1472977c08185f3501f66878ab29abd5cce1
Ederley <Ederley@DESKTOP-B7L27UQ>
2019-10-07 20:23:19 -0300

    Ederley novo metodo listarGuardas

--- src/Interface/UsuarioDao.java
+++ src/Interface/UsuarioDao.java
@@ -11,29 +11,31 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 //import ronaldo.bancodados.CriaConexao;
 //import ronaldo.logica.Contato;
-
-
 
 /**
  *
  * @author Lemes
  */
 public class UsuarioDao {
-    
-     
+
     private Connection conexao;
-    
-    public UsuarioDao() throws SQLException{
+    Connection con = null;
+    String sql;
+    ResultSet rs;
+    PreparedStatement pstm;
+
+    public UsuarioDao() throws SQLException {
         this.conexao = CriaConexao.getConexao();
     }
-    
-    public void adiciona (Usuario c1) throws SQLException{
+
+    public void adiciona(Usuario c1) throws SQLException {
         // prepara a conexao com oo banco
         String sql = "insert into guardas (matricula, nome, endereco, telefone, email, sexo, situacao )" + "values (?,?,?,?,?,?,?)";
         PreparedStatement stmt = conexao.prepareStatement(sql);
-        
+
         // seta os valores 
         stmt.setLong(1, c1.getMatricula());
         stmt.setString(2, c1.getNome());
@@ -42,23 +44,23 @@ public class UsuarioDao {
         stmt.setString(5, c1.getEmail());
         stmt.setString(6, c1.getSexo());
         stmt.setString(7, c1.getSituacao());
-        
+
         // executa o codigo sql
         stmt.execute();
         stmt.close();
     }
-    
-    public List<Usuario> getLista(String nome) throws SQLException{
+
+    public List<Usuario> getLista(String nome) throws SQLException {
         System.out.println("chamada do metodo:  public List<Usuario> getLista(String nome) ");
-        
+
         String sql = "select * from guardas where nome like ?";
         PreparedStatement stmt = this.conexao.prepareStatement(sql);
         stmt.setString(1, nome);
         ResultSet rs = stmt.executeQuery();
-        
+
         List<Usuario> minhaLista = new ArrayList<Usuario>();
-        
-        while(rs.next()){
+
+        while (rs.next()) {
             Usuario c1 = new Usuario();
             c1.setIdUsuario(rs.getInt("id"));
             c1.setMatricula(Long.valueOf(rs.getString("matricula")));
@@ -69,17 +71,17 @@ public class UsuarioDao {
             c1.setSexo(rs.getString("sexo"));
             c1.setSituacao(rs.getString("situacao"));
             minhaLista.add(c1);
-       }
+        }
         rs.close();
         stmt.close();
         return minhaLista;
     }
-    
-       public void altera (Usuario c1) throws SQLException{
-        String sql = "update guardas set nome=?, endereco=?, telefone=?, email=?" +
-                ",sexo=?, situacao=? where matricula=?";
+
+    public void altera(Usuario c1) throws SQLException {
+        String sql = "update guardas set nome=?, endereco=?, telefone=?, email=?"
+                + ",sexo=?, situacao=? where matricula=?";
         PreparedStatement stmt = conexao.prepareStatement(sql);
-        
+
         // seta os valores 
         stmt.setString(1, c1.getNome());
         stmt.setString(2, c1.getEndereco());
@@ -88,19 +90,51 @@ public class UsuarioDao {
         stmt.setString(5, c1.getSexo());
         stmt.setString(6, c1.getSituacao());
         stmt.setLong(7, c1.getMatricula());
-        
+
         // executa o codigo sql
         stmt.execute();
         stmt.close();
-        
+
     }
-    
-    public void remove(Usuario c1) throws SQLException{
+
+    public void remove(Usuario c1) throws SQLException {
         String sql = "delete from guardas where matricula=?";
         PreparedStatement stmt = conexao.prepareStatement(sql);
         stmt.setLong(1, c1.getMatricula());
         stmt.execute();
         stmt.close();
-          
+
     }
+
+    public List<Usuario> listarGuardas() {
+        ArrayList<Usuario> listUser = new ArrayList<>();
+
+        try {
+
+            con = CriaConexao.getConexao();
+            sql = "SELECT nome FROM guardas group by id order by id asc";
+            pstm = con.prepareStatement(sql);
+            rs = pstm.executeQuery(sql);
+
+            while (rs.next()) {
+
+                Usuario user = new Usuario();
+
+                user.setNome(rs.getString("nome"));
+                //user.setCatDescricao(rs.getString(2));
+                //user.setCatValorDiaria(rs.getDouble(3));           
+                listUser.add(user);
+       
+            }
+
+        } catch (Exception erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro PSTM " + erro.getMessage());
+
+        }
+
+        return listUser;
+
+    }
+
 }
