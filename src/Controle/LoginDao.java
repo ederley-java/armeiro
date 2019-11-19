3eae257833b2915ac08df9aa3d8638906d83bfc3
Marcio Lemes <marcio.aflemes@gmail.com>
2019-11-19 16:56:48 -0200

    concluida a etapa de pesquisar no banco e chamar a tela principal

--- src/Controle/LoginDao.java
+++ src/Controle/LoginDao.java
@@ -6,8 +6,12 @@
 package Controle;
 
 import Interface.CriaConexao;
+import Interface.Principal;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,16 +19,34 @@ import java.sql.SQLException;
  */
 public class LoginDao {
     
-     private Connection conexao;
+    private Connection conexao;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
     
     public LoginDao() throws SQLException{
         this.conexao = CriaConexao.getConexao();
     }
-    
-    
+        
      public void logar(LoginBean c1){
-         String sql = "d from guardas where matricula=?";
-         
+        String sql = "select * from login where usuario=? and senha=?";
+        try{
+            pst = conexao.prepareStatement(sql);
+            pst.setString(1, c1.getUsuario());
+            pst.setString(2, c1.getSenha());
+            
+            rs = pst.executeQuery();
+            if (rs.next()){
+                Principal tela = new Principal();
+                tela.setVisible(true);
+            }else{
+                JOptionPane.showMessageDialog(null,"Usuário ou Senha Inválido!");
+            }
+           
+        }
+        catch (Exception e){
+            
+        }
+        
          
      }
     
